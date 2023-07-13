461b3ae32d45a21b024e7805ac18bdef9307c549
YongChan Cho <h3236516@gmail.com>
2023-07-13 15:59:43 +0900

    feat(login): add login page

--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -1,23 +1,67 @@
-import { GetServerSideProps, NextPage } from "next";
+import { client } from "@/utils/client";
+import { Button, Card, CardHeader, Container, TextField } from "@mui/material";
+import { NextPage } from "next";
+import Router from 'next/router';
 import { useState } from "react";
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    const { redirectTo } = ctx.query;
-
-    return {
-        props: {
-            redirectTo: redirectTo || "/",
-        },
-    };
-};
 
 interface LoginProps {
     redirectTo: string;
 }
 
 const Login: NextPage<LoginProps> = ({ redirectTo }) => {
-    const [popup, setPopup] = useState<Window | null>();
-    const [id, setId] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [loginLoading, setLoginLoading] = useState<boolean>(false);
+    const [id, setId] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const handleLogin = (id, password) => {
+        client.post('/admin/login', { id: id, password: password });
+        Router.push('/admin');
+    }
+
+    return (
+        <>
+            <Card style={{
+                width: "100vw",
+                height: "100vh",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}>
+                <Container maxWidth="sm" style={{
+                    display: "flex",
+                    flexDirection: "column",
+                }}>
+                    <CardHeader title="로그인" />
+                    <TextField
+                        required
+                        type="text"
+                        id="outlined-required"
+                        label="아이디"
+                        style={{
+                            marginBottom: "10px"
+                        }}
+                        onChange={(e) => setId(e.target.value)}
+                    />
+                    <TextField
+                        id="outlined-password-input"
+                        label="비밀번호"
+                        type="password"
+                        autoComplete="current-password"
+                        style={{
+                            marginBottom: "10px"
+                        }}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button
+                        sx={{ margin: 1 }}
+                        variant="contained"
+                        onClick={() => {
+                            handleLogin(id, password);
+                        }}
+                    >로그인</Button>
+                </Container>
+            </Card>
+        </>
+    )
 }
+
+export default Login;
