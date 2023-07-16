62b4254687cb0975ed9cc81c9b3c017728423797
YongChan Cho <h3236516@gmail.com>
2023-07-16 23:23:10 +0900

    feat(info): add user profile

--- src/content/user/info/Info.tsx
+++ src/content/user/info/Info.tsx
@@ -1,6 +1,6 @@
 import { User } from "@/models/user";
 import { swrFetcher } from "@/utils/client";
-import { Box, Card, Grid, Typography } from "@mui/material";
+import { Avatar, Box, Card, Grid, Typography } from "@mui/material";
 import dayjs from "dayjs";
 import Status404 from "pages/404";
 import useSWR from "swr";
@@ -29,51 +29,71 @@ const Info: React.FC<InfoProps> = ({ userId: info }) => {
                             유저 정보
                         </Typography>
                         <Box>
-                            <Typography variant="h1" gutterBottom>
-                                {userInfo.name}
-                            </Typography>
-                            <Typography
-                                variant="h4"
-                                fontWeight="normal"
-                                color="text.secondary"
-                            >
-                                ID: {userInfo.id}
-                            </Typography>
-                            <Typography
-                                variant="h4"
-                                fontWeight="normal"
-                                color="text.secondary"
-                            >
-                                이메일: {userInfo.email ? userInfo.email : "미등록"}
-                            </Typography>
-                            <Typography
-                                variant="h4"
-                                fontWeight="normal"
-                                color="text.secondary"
-                            >
-                                전화번호: {userInfo.phone ? userInfo.phone : "미등록"}
-                            </Typography>
-                            <Typography
-                                variant="h4"
-                                fontWeight="normal"
-                                color="text.secondary"
-                            >
-                                가입일: {dayjs(userInfo.createdAt).format("YYYY-MM-DD HH:mm:ss")}
-                            </Typography>
-                            <Typography
-                                variant="h4"
-                                fontWeight="normal"
-                                color="text.secondary"
-                            >
-                                로그인: {userInfo.provider}
-                            </Typography>
-                            <Typography
-                                variant="h4"
-                                fontWeight="normal"
-                                color="text.secondary"
-                            >
-                                학교ID: {userInfo.userSchoolId ? userInfo.userSchoolId : "인증되지 않음"}
-                            </Typography>
+                            <Grid direction="row" container spacing={4}>
+                                <Grid item xs={12}>
+                                    <Typography variant="h1" gutterBottom>
+                                        {userInfo.name}
+                                    </Typography>
+                                    <Avatar
+                                        sx={{
+                                            mr: 2,
+                                            width: 200,
+                                            height: 200
+                                        }}
+                                        variant="rounded"
+                                        alt={userInfo.name}
+                                        src={
+                                            userInfo.profile ?
+                                                process.env.NEXT_PUBLIC_S3_URL + '/' + userInfo.profile :
+                                                "https://media.discordapp.net/attachments/1123544235992416292/1130140456374780025/1.png?width=880&height=880"
+                                        }
+                                    />
+                                </Grid>
+                                <Grid item lg={8} xs={12}>
+                                    <Typography
+                                        variant="h4"
+                                        fontWeight="normal"
+                                        color="text.secondary"
+                                    >
+                                        ID: {userInfo.id}
+                                    </Typography>
+                                    <Typography
+                                        variant="h4"
+                                        fontWeight="normal"
+                                        color="text.secondary"
+                                    >
+                                        이메일: {userInfo.email ? userInfo.email : "미등록"}
+                                    </Typography>
+                                    <Typography
+                                        variant="h4"
+                                        fontWeight="normal"
+                                        color="text.secondary"
+                                    >
+                                        전화번호: {userInfo.phone ? userInfo.phone : "미등록"}
+                                    </Typography>
+                                    <Typography
+                                        variant="h4"
+                                        fontWeight="normal"
+                                        color="text.secondary"
+                                    >
+                                        가입일: {dayjs(userInfo.createdAt).format("YYYY-MM-DD HH:mm:ss")}
+                                    </Typography>
+                                    <Typography
+                                        variant="h4"
+                                        fontWeight="normal"
+                                        color="text.secondary"
+                                    >
+                                        로그인: {userInfo.provider}
+                                    </Typography>
+                                    <Typography
+                                        variant="h4"
+                                        fontWeight="normal"
+                                        color="text.secondary"
+                                    >
+                                        학교ID: {userInfo.userSchoolId ? userInfo.userSchoolId : "인증되지 않음"}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
                         </Box>
                     </Box>
                 </Grid>
