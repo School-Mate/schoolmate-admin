e5df16b26b56adacb929baf053e9a5a9b229b722
YongChan Cho <h3236516@gmail.com>
2023-12-11 13:51:02 +0900

    feat(allschools): add school name change button

--- src/content/school/all/AllSchoolsTable.tsx
+++ src/content/school/all/AllSchoolsTable.tsx
@@ -4,9 +4,16 @@ import PropTypes from 'prop-types';
 import Router from "next/router";
 import { client } from "@/utils/client";
 import { School } from "schoolmate-types";
+import { Input } from '@mui/material';
 
 interface AllSchoolTableProps {
     allSchools: School[];
+}
+
+interface NameDialogProps {
+    onClose: () => void;
+    open: boolean;
+    school: School;
 }
 
 const applyPagination = (schools: School[], page: number, limit: number) => {
@@ -16,6 +23,8 @@ const applyPagination = (schools: School[], page: number, limit: number) => {
 const AllSchoolsTable: FC<AllSchoolTableProps> = ({ allSchools: allSchools }) => {
     const [page, setPage] = useState<number>(0);
     const [limit, setLimit] = useState<number>(5);
+    const [targetSchool, setTargetSchool] = useState<School>();
+    const [nameOpen, setNameOpen] = useState(false);
 
     const handlePageChange = (event: unknown, newPage: number) => {
         setPage(newPage);
@@ -25,16 +34,67 @@ const AllSchoolsTable: FC<AllSchoolTableProps> = ({ allSchools: allSchools }) =>
         setLimit(parseInt(event.target.value));
     }
 
-    const handleNameButton = async (schoolId: string, name: string) => {
-        try {
-            await client.post(`/admin/${schoolId}/name`, { name: name });
+    const paginatedAllSchools = applyPagination(allSchools, page, limit);
 
-        } catch (error) {
-            console.log(error);
-        }
+    const handleNameOpen = (school: School) => {
+        setTargetSchool(school);
+        setNameOpen(true);
     }
 
-    const paginatedAllSchools = applyPagination(allSchools, page, limit);
+    const handleNameClose = () => {
+        setNameOpen(false);
+    }
+
+    const NameDialog = (props: NameDialogProps) => {
+        const { onClose, open, school } = props;
+        if (!school) return (<></>);
+        const [newName, setNewName] = useState(school.name);
+
+        const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+            setNewName(event.target.value);
+        }
+
+        const handleNameClose = () => {
+            onClose();
+        }
+
+        const handleNameSubmit = async () => {
+            try {
+                await client.post(`/admin/${school.schoolId}/name`, { name: newName });
+                onClose();
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        return (
+            <Dialog
+                onClose={handleNameClose}
+                open={open}
+            >
+                <Box p={2}>
+                    <h2>학교명 설정</h2>
+                    <p>학교명을 설정합니다.</p>
+                    <Box p={2}>
+                        <Input
+                            onChange={handleNameChange}
+                            placeholder={school.defaultName}
+                            type="text"
+                            value={newName}
+                        />
+                    </Box>
+                    <Box p={2}>
+                        <Button
+                            onClick={handleNameSubmit}
+                            variant="outlined"
+                        >
+                            학교명 설정
+                        </Button>
+                    </Box>
+                </Box>
+            </Dialog>
+        )
+    }
 
     return (
         <>
@@ -66,7 +126,7 @@ const AllSchoolsTable: FC<AllSchoolTableProps> = ({ allSchools: allSchools }) =>
                                     <TableCell align="right">
                                         <Button
                                             variant="outlined"
-                                        // onClick={() => handleContentOpen(school)}
+                                            onClick={() => handleNameOpen(school)}
                                         >
                                             학교명 설정
                                         </Button>
@@ -88,6 +148,12 @@ const AllSchoolsTable: FC<AllSchoolTableProps> = ({ allSchools: allSchools }) =>
                     />
                 </Box>
             </Card>
+
+            <NameDialog
+                open={nameOpen}
+                onClose={handleNameClose}
+                school={targetSchool}
+            />
         </>
     )
 }
