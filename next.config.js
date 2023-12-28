29cf924a2654e26ede8b2c86524b8c97d9437d15
kiss8981 <kdh89811@gmail.com>
2023-12-28 13:00:17 +0800

    fix: image url

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { typescript: { ignoreBuildErrors: true } };
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  images: { domains: ["d184yr88ghlmna.cloudfront.net"] },
+};
 
 module.exports = nextConfig;
